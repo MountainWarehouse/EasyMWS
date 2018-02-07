abf26a16632c3f6f1f31df1b6ca925064340f487
Rob Church <rob.church@mountainwarehouse.com>
2018-02-07 19:51:18 +0000

    Add MarketplaceWebService code from Amazon

---
+++ src/EasyMWS/MarketplaceWebService/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarketplaceWebService CSharp Library")]
+[assembly: AssemblyProduct("MarketplaceWebService")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+ 
