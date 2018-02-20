5f944dbd39be4659479b4efbe3c95d2f18d1bf6e
Rob Church <rob.church@mountainwarehouse.com>
2018-02-20 18:56:36 +0000

    Create CallbackActivator and tests

---
+++ src/EasyMWS/EasyMWS.Tests/CallbackActivatorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MountainWarehouse.EasyMWS;
+using NUnit.Framework;
+
+namespace EasyMWS.Tests
+{
+	[TestFixture]
+	public class CallbackActivatorTests
+	{
+		private Stream _passedStream;
+		private bool _called;
+		private CallbackActivator _callbackActivator;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_callbackActivator = new CallbackActivator();
+
+			// Set up data that will be set by the static method activated.
+			_called = false;
+			_passedStream = null;
+		}
+
+		[Test]
+		public void SerializeCommand_GeneratesData()
+		{
+
+		}
+
+
+
+
+		public static void TestMethod()
+		{
+
+		}
+
+
+	}
+}
