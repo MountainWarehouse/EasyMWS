bf5eed03b8e800ae7577d09442d195eaf8b916ca
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-02-23 15:08:15 +0000

    Added name column to the report table and created migrations

---
+++ src/EasyMWS/EasyMWS/Migrations/20180223145618_AddColumnReportName.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MountainWarehouse.EasyMWS.Migrations
+{
+    public partial class AddColumnReportName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Reports",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Reports");
+        }
+    }
+}
