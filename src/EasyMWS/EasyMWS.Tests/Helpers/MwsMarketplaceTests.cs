a777c22eb095f92d79dddbb9a2fb2eee01b2b037
Gabriel Cazan <gabriel.cazan@mountainwarehouse.com>
2018-03-06 12:40:11 +0000

    Added tests for MwsMarketplace helper

---
+++ src/EasyMWS/EasyMWS.Tests/Helpers/MwsMarketplaceTests.cs
@@ -0,0 +1,41 @@
+﻿using MountainWarehouse.EasyMWS.Helpers;
+using NUnit.Framework;
+
+namespace EasyMWS.Tests.Helpers
+{
+    public class MwsMarketplaceTests
+    {
+	    [Test]
+	    public void Canada_IsInitializedWithExpectedData()
+	    {
+		    var marketplace = MwsMarketplace.Canada;
+
+		    Assert.AreEqual("Canada", marketplace.Name);
+		    Assert.AreEqual("CA", marketplace.CountryCode);
+		    Assert.AreEqual("A2EUQ1WTGCTBG2", marketplace.Id);
+		    Assert.AreEqual("https://mws.amazonservices.com", marketplace.MwsEndpoint);
+	    }
+
+	    [Test]
+	    public void US_IsInitializedWithExpectedData()
+	    {
+		    var marketplace = MwsMarketplace.US;
+
+		    Assert.AreEqual("US", marketplace.Name);
+		    Assert.AreEqual("US", marketplace.CountryCode);
+		    Assert.AreEqual("ATVPDKIKX0DER", marketplace.Id);
+		    Assert.AreEqual("https://mws.amazonservices.com", marketplace.MwsEndpoint);
+	    }
+
+	    [Test]
+	    public void UK_IsInitializedWithExpectedData()
+	    {
+		    var marketplace = MwsMarketplace.UK;
+
+		    Assert.AreEqual("UK", marketplace.Name);
+		    Assert.AreEqual("UK", marketplace.CountryCode);
+		    Assert.AreEqual("A1F83G8C2ARO7P", marketplace.Id);
+		    Assert.AreEqual("https://mws-eu.amazonservices.com", marketplace.MwsEndpoint);
+	    }
+	}
+}
