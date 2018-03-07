c0934dcebbeb52631d5c2c1eeda1b1c96da0790e
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-03-07 17:00:46 +0000

    Created new serializable PropertiesContainer to be returned by ...
    ReportRequestFactories

---
+++ src/EasyMWS/EasyMWS/Helpers/ReportRequestPropertiesContainer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using MarketplaceWebService.Model;
+
+namespace MountainWarehouse.EasyMWS.Helpers
+{
+	[Serializable]
+	public class ReportRequestPropertiesContainer
+	{
+		#region Properties required for requesting all amazon MWS reports
+
+		public string ReportType { get; set; }
+
+		public string Merchant { get; set; }
+
+		public string MwsAuthToken { get; set; }
+
+		#endregion
+
+		#region Optional properties only used by some amazon MWS reports
+
+		public List<string> MarketplaceIdList { get; set; }
+
+		#endregion
+
+
+		/// <summary>
+		/// The frequency with which amazon updates information for the Report request type attached to this report request wrapper.<para />
+		/// This information can be used to reduce the number of report request made, especially if the requests are redundant.<para/>
+		/// If the content for a report type is only updated Daily by amazon, then it shouldn't be requested on a base more frequent than daily. 
+		/// </summary>
+		[NonSerialized]
+		public ContentUpdateFrequency UpdateFrequency;
+
+		/// <summary>
+		/// Prevent clients from bypassing setting required properties as defined by the public constructor
+		/// </summary>
+		private ReportRequestPropertiesContainer()
+		{
+		}
+
+		/// <summary>
+		/// Creates a new RequestReportRequest wrapper object that also contains additional information regarding the request object.
+		/// </summary>
+		/// <param name="reportType"></param>
+		/// <param name="merchant"></param>
+		/// <param name="mwsAuthToken"></param>
+		/// <param name="marketplaceIdList"></param>
+		/// <param name="updateFrequency"></param>
+		public ReportRequestPropertiesContainer(string reportType, string merchant, string mwsAuthToken, ContentUpdateFrequency updateFrequency, List<string> marketplaceIdList = null) =>
+			(ReportType, Merchant, MwsAuthToken, MarketplaceIdList, UpdateFrequency) = (reportType, merchant, mwsAuthToken, marketplaceIdList, updateFrequency);
+	}
+
+	/// <summary>
+	/// Extension methods for the ReportRequestPropertiesContainer class
+	/// </summary>
+	public static class ReportRequestSerializablePropertiesExtensions
+	{
+		/// <summary>
+		/// Converts the current container of MWS report request properties into an object of type RequestReportRequest, required for submission by the MWS client. 
+		/// </summary>
+		/// <param name="serializablePropertiesObject"></param>
+		public static RequestReportRequest ToMwsClientReportRequest(this ReportRequestPropertiesContainer serializablePropertiesObject)
+		{
+			if (serializablePropertiesObject == null) return null;
+
+			return new RequestReportRequest
+			{
+				MWSAuthToken = serializablePropertiesObject.MwsAuthToken,
+				Merchant = serializablePropertiesObject.Merchant,
+				ReportType = serializablePropertiesObject.ReportType,
+
+				MarketplaceIdList = serializablePropertiesObject.MarketplaceIdList == null ? null : new IdList { Id = serializablePropertiesObject.MarketplaceIdList }
+			};
+		}
+	}
+
+	/// <summary>
+	/// Describes how often content is updated for an amazon MWS web service endpoint.
+	/// </summary>
+	public enum ContentUpdateFrequency
+	{
+		NearRealTime,
+		Daily,
+		Unknown
+	}
+}
+
