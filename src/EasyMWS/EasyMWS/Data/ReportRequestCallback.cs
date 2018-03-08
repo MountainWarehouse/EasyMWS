4c7f328e3fa24b371607b44347a3d8e6b4783ee9
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-03-08 11:52:34 +0000

    Deleted Report table and created ReportRequestCallbacks table
    because a Report table is not actually needed.
    I just deleted the previous EF migrations, since they were minor, and nothing was needed from them.

---
+++ src/EasyMWS/EasyMWS/Data/ReportRequestCallback.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MountainWarehouse.EasyMWS.Helpers;
+
+namespace MountainWarehouse.EasyMWS.Data
+{
+    public class ReportRequestCallback
+    {
+	    [Key]
+	    public int Id { get; set; }
+	    public string TypeName { get; set; }
+	    public string MethodName { get; set; }
+	    public string Data { get; set; }
+	    public string DataTypeName { get; set; }
+
+
+	    public string ReportType { get; set; }
+	    public AmazonRegion AmazonRegion { get; set; }
+	    public ContentUpdateFrequency ContentUpdateFrequency { get; set; }
+	    public DateTime? LastRequested { get; set; }
+	}
+}
--- src/EasyMWS/EasyMWS/Migrations/20180308115015_InitialCreate.Designer.cs
+++ src/EasyMWS/EasyMWS/Migrations/20180308115015_InitialCreate.Designer.cs
@@ -4,13 +4,16 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
+using MountainWarehouse.EasyMWS;
 using MountainWarehouse.EasyMWS.Data;
+using MountainWarehouse.EasyMWS.Helpers;
 using System;
 
 namespace MountainWarehouse.EasyMWS.Migrations
 {
     [DbContext(typeof(EasyMwsContext))]
-    [Migration("20180220215544_InitialCreate")]
+    [Migration("20180308115015_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -20,14 +23,30 @@ namespace MountainWarehouse.EasyMWS.Migrations
                 .HasAnnotation("ProductVersion", "2.0.1-rtm-125")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("MountainWarehouse.EasyMWS.Data.Report", b =>
+            modelBuilder.Entity("MountainWarehouse.EasyMWS.Data.ReportRequestCallback", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
+                    b.Property<int>("AmazonRegion");
+
+                    b.Property<int>("ContentUpdateFrequency");
+
+                    b.Property<string>("Data");
+
+                    b.Property<string>("DataTypeName");
+
+                    b.Property<DateTime?>("LastRequested");
+
+                    b.Property<string>("MethodName");
+
+                    b.Property<string>("ReportType");
+
+                    b.Property<string>("TypeName");
+
                     b.HasKey("Id");
 
-                    b.ToTable("Reports");
+                    b.ToTable("ReportRequestCallbacks");
                 });
 #pragma warning restore 612, 618
         }
