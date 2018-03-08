00514b3717e201b21b7041464a27d31f3a41fb05
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-03-08 10:21:18 +0000

    Added unit test that makes sure that ReportRequestPropertiesContainer...
    ... can be serialized and deserialized successfully using callbackActivator.

--- src/EasyMWS/EasyMWS.Tests/Helpers/ReportRequestPropertiesContainerTests.cs
+++ src/EasyMWS/EasyMWS.Tests/Helpers/ReportRequestPropertiesContainerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using MountainWarehouse.EasyMWS;
 using MountainWarehouse.EasyMWS.Helpers;
 using NUnit.Framework;
 
@@ -10,7 +11,19 @@ namespace EasyMWS.Tests.Helpers
 {
     public class ReportRequestPropertiesContainerTests
     {
-	    [Test]
+	    private static bool _called;
+	    private CallbackActivator _callbackActivator;
+	    private static ReportRequestPropertiesContainer _callbackData;
+
+		[SetUp]
+	    public void SetUp()
+	    {
+		    _callbackActivator = new CallbackActivator();
+			_called = false;
+			_callbackData = new ReportRequestPropertiesContainer("", "", "", ContentUpdateFrequency.Daily, new List<string>());
+		}
+
+		[Test]
 	    public void ReportRequestPropertiesContainer_IsSerializedAndDeserialized_AsExpected()
 	    {
 		    var testReportType = "test report type 123";
@@ -35,5 +48,36 @@ namespace EasyMWS.Tests.Helpers
 		    Assert.AreEqual(propertiesContainer.UpdateFrequency, deserializedPropertiesContainer.UpdateFrequency);
 			CollectionAssert.AreEqual(propertiesContainer.MarketplaceIdList, deserializedPropertiesContainer.MarketplaceIdList);
 		}
+
+		[Test]
+	    public void ReportRequestPropertiesContainer_IsSerializedAndDeserializedProperly_UsingCallbackActivator()
+	    {
+		    var testReportType = "test report type 123";
+		    var testMerchant = "test merchant 123";
+		    var testMwsAuthToken = "test auth token 123";
+		    var marketplaceIdList = new List<string> { "asdf1234", "tyui5678", "vbnm4567" };
+		    ContentUpdateFrequency testUpdateFrequency = ContentUpdateFrequency.Daily;
+		    var propertiesContainer = new ReportRequestPropertiesContainer(testReportType, testMerchant, testMwsAuthToken, testUpdateFrequency, marketplaceIdList);
+
+			var serialized = _callbackActivator.SerializeCallback(TestMethod, propertiesContainer);
+		    _callbackActivator.CallMethod(serialized);
+		    Assert.IsTrue(_called);
+
+			Assert.AreEqual(testReportType, _callbackData.ReportType);
+		    Assert.AreEqual(testMerchant, _callbackData.Merchant);
+		    Assert.AreEqual(testMwsAuthToken, _callbackData.MwsAuthToken);
+		    Assert.AreEqual(testUpdateFrequency, _callbackData.UpdateFrequency);
+		    CollectionAssert.AreEqual(marketplaceIdList, _callbackData.MarketplaceIdList);
+		}
+
+	    public static void TestMethod(Stream stream, object callbackData)
+	    {
+		    _called = true;
+		    if (callbackData is ReportRequestPropertiesContainer test)
+		    {
+			    _callbackData = test;
+		    }
+
+	    }
 	}
 }
