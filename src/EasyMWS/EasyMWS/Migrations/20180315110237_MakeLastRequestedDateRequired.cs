3d308b71478c2010f05ef52068cbdf19302543d7
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-03-15 11:04:07 +0000

    Make LastRequested date field required

---
+++ src/EasyMWS/EasyMWS/Migrations/20180315110237_MakeLastRequestedDateRequired.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MountainWarehouse.EasyMWS.Migrations
+{
+    public partial class MakeLastRequestedDateRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastRequested",
+                table: "ReportRequestCallbacks",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastRequested",
+                table: "ReportRequestCallbacks",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
