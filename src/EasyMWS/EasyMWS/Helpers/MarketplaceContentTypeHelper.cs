e50a7ac55e6dc988cb1aad27f9432a4c22e513a4
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-03-16 15:26:41 +0000

    Added amazon feed content-type helper that can provide the correct ..
    .. content-type that should be used to submit amazon feeds for a given AmazonRegion. Please not that the amazon documentation does not specify what content-type should be used for the following AmazonRegions : India, Australia, Brazil. A little trial & fail should be done in order to find out the correct content-type expected when submitting feeds for these regions.

---
+++ src/EasyMWS/EasyMWS/Helpers/MarketplaceContentTypeHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MountainWarehouse.EasyMWS.Helpers
+{
+	/// <summary>
+	/// More information can be found at : https://docs.developer.amazonservices.com/en_US/feeds/Feeds_SubmitFeed.html
+	/// </summary>
+	public class MarketplaceContentTypeHelper
+    {
+	    public string GetContentTypeForXmlFiles()
+	    {
+		    return "text/xml";
+	    }
+
+	    public List<string> GetSupportedContentTypesForFlatFiles(AmazonRegion region)
+	    {
+		    if (region == AmazonRegion.NorthAmerica || region == AmazonRegion.Europe)
+		    {
+			    return new List<string> {"text/tab-separated-values; charset=iso-8859-1"};
+		    }
+		    else if (region == AmazonRegion.Japan)
+		    {
+			    return new List<string> {"text/tab-separated-values; charset=Shift_JIS"};
+		    }
+		    else if (region == AmazonRegion.China)
+		    {
+			    return new List<string> {"text/tab-separated-values;charset=UTF-8", "text/tab-separated-values;charset=UTF-16" };
+		    }
+		    else
+		    {
+			    throw new NotSupportedException(
+				    $"Amazon region '{region.ToString()}' not supported by EasyMwsClient due to unknown ContentType that should be used to submit a feed to amazon.");
+		    }
+	    }
+    }
+}
