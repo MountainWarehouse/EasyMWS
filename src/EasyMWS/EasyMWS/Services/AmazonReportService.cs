fe8c0d45bfb2b4c3d56d6c62c7ea82693c1dbff4
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-03-29 16:01:36 +0100

    Implement public behaviour that provides access to any reports stored
    internally after they're downloaded from amazon. This behavior can be used by users of the EasyMwsClient if they enable the KeepAmazonReportsInLocalDbAfterCallbackIsPerformed option for the client.

--- src/EasyMWS/EasyMWS/Services/AmazonReportService.cs
+++ src/EasyMWS/EasyMWS/Services/AmazonReportService.cs
@@ -1,11 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using MountainWarehouse.EasyMWS.Data;
 using MountainWarehouse.EasyMWS.Repositories;
 
 namespace MountainWarehouse.EasyMWS.Services
 {
-    internal class AmazonReportService : IAmazonReportService
-    {
+    internal class AmazonReportService : IAmazonReportService, IInternalStorageReportService
+	{
 	    private readonly IAmazonReportRepo _amazonReportRepo;
 
 	    internal AmazonReportService(IAmazonReportRepo reportRepo) : this() => (_amazonReportRepo) = (reportRepo);
@@ -15,5 +18,16 @@ namespace MountainWarehouse.EasyMWS.Services
 		public void Delete(AmazonReport report) => _amazonReportRepo.Delete(report);
 		public IQueryable<AmazonReport> GetAll() => _amazonReportRepo.GetAll();
 		public void SaveChanges() => _amazonReportRepo.SaveChanges();
+		public IEnumerable<AmazonReport> GetAllReports() => GetAll();
+		public IQueryable<AmazonReport> GetReportsWhere(Expression<Func<AmazonReport, bool>> predicate) => GetAll().Where(predicate);
+
+		public IEnumerable<AmazonReport> GetAllReportsWithType(string reportType) => GetAll().Where(r => r.ReportType == reportType);
+
+		public void DeleteAllReportsFromInternalStorage()
+		{
+			var reportsToDelete = GetAll();
+			_amazonReportRepo.DeleteRange(reportsToDelete);
+			_amazonReportRepo.SaveChanges();
+		}
 	}
 }
