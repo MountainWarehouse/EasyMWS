99f51ab63ab2a64606ca66e84fecb958d0b4a026
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2018-04-09 17:03:43 +0100

    Added a version of EasyMws client that can expose events instead of
    relying on method callbacks.

---
+++ src/EasyMWS/EasyMWS/Client/IEasyMwsClientWithEvents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MountainWarehouse.EasyMWS.Model;
+
+namespace MountainWarehouse.EasyMWS.Client
+{
+    public interface IEasyMwsClientWithEvents
+    {
+	    event EventHandler<ReportDownloadedEventArgs> ReportDownloaded;
+	    event EventHandler<FeedSubmittedEventArgs> FeedSubmitted;
+		void Poll();
+
+	    void QueueReport(ReportRequestPropertiesContainer reportRequestContainer);
+
+	    void QueueFeed(FeedSubmissionPropertiesContainer feedSubmissionContainer);
+	}
+}
