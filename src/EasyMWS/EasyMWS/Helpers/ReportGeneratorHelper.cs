8a755aaaf3863a68ab98a3d068c4655f3f4516ae
Gabriel Cazan <cazan.p.gabriel@gmail.com>
2019-03-28 12:14:48 +0000

    Fetch the permitted marketplaces from the mapper instead of args

--- src/EasyMWS/EasyMWS/Helpers/ReportGeneratorHelper.cs
+++ src/EasyMWS/EasyMWS/Helpers/ReportGeneratorHelper.cs
@@ -9,26 +9,25 @@ namespace MountainWarehouse.EasyMWS.Helpers
 	internal static class ReportGeneratorHelper
 	{
 		public static ReportRequestPropertiesContainer GenerateReportRequest(string reportType,
-			ContentUpdateFrequency reportUpdateFrequency,
-			IEnumerable<string> permittedMarketplacesIds, IEnumerable<string> requestedMarketplaces = null, DateTime? startDate = null,
+			ContentUpdateFrequency reportUpdateFrequency, IEnumerable<string> requestedMarketplaces = null, DateTime? startDate = null,
 			DateTime? endDate = null, ReportOptions reportOptions = null)
 		{
-			ValidateMarketplaceCompatibility(reportType, permittedMarketplacesIds, requestedMarketplaces);
+			ValidateMarketplaceCompatibility(reportType, requestedMarketplaces);
 			return new ReportRequestPropertiesContainer(reportType, reportUpdateFrequency, requestedMarketplaces, startDate,
 				endDate, reportOptions?.GetOptionsString());
 		}
 
 		public static ReportRequestPropertiesContainer GenerateReportRequest(string reportType, ContentUpdateFrequency reportUpdateFrequency,
-			IEnumerable<MwsMarketplace> permittedMarketplaces, IEnumerable<MwsMarketplace> requestedMarketplaces = null, 
+			IEnumerable<MwsMarketplace> requestedMarketplaces = null, 
 			DateTime? startDate = null, DateTime? endDate = null, ReportOptions reportOptions = null)
-			=> GenerateReportRequest(reportType, reportUpdateFrequency, 
-				permittedMarketplaces.Select(m => m.Id), requestedMarketplaces.Select(m => m.Id), 
+			=> GenerateReportRequest(reportType, reportUpdateFrequency, requestedMarketplaces.Select(m => m.Id), 
 				startDate, endDate, reportOptions);
 
-		private static void ValidateMarketplaceCompatibility(string reportType, IEnumerable<string> permittedMarketplacesIds,
-			IEnumerable<string> requestedMarketplacesIds = null)
+		private static void ValidateMarketplaceCompatibility(string reportType, IEnumerable<string> requestedMarketplacesIds = null)
 		{
 			if (requestedMarketplacesIds == null) return;
+
+			var permittedMarketplacesIds = ReportsPermittedMarketplacesMapper.GetMarketplaces(reportType)?.Select(m => m.Id) ?? MwsMarketplaceGroup.AmazonGlobal().Select(m => m.Id);
 
 			foreach (var requestedMarketplace in requestedMarketplacesIds)
 			{
